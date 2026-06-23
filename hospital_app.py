9d015236152193de3ce1c2311d0d2d60c7087f0d
student-devi <studentbtb1@gmail.com>
2026-06-23 15:03:23 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -1,26 +1,26 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle 
+import pickle
 import os
 
 st.set_page_config(page_title="Smart Hospital Patient Navigator", page_icon="🏥", layout="wide")
 
 @st.cache_resource
 def load_model():
-  with open('hospital_model.pkl', 'rb') as f:
-    return pickle.load(f)
+    with open('hospital_model.pkl', 'rb') as f:
+        return pickle.load(f)
 
-bundle = load_model()
-model = bundle['model']
-scaler = bundle['scaler']
-features = bundle['features']
-cols_to_scale = bundle['features']
-dept_map_inv = bundle['dept_map_inv']
-gender_map = bundle['gender_map']
-temp_map = bundle['temp_map']
-hr_map = bundle['hr_map']
-dur_map = bundle['dur_map']
-cc_map = bundle['cc_map']
+bundle        = load_model()
+model         = bundle['model']
+scaler        = bundle['scaler']
+features      = bundle['features']
+cols_to_scale = bundle['cols_to_scale']
+dept_map_inv  = bundle['dept_map_inv']
+gender_map    = bundle['gender_map']
+temp_map      = bundle['temp_map']
+hr_map        = bundle['hr_map']
+dur_map       = bundle['dur_map']
+cc_map        = bundle['cc_map']
 
 
