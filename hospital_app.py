ba8a268e39bcb7a3a61e1c9002be12596b7b4a98
student-devi <studentbtb1@gmail.com>
2026-06-23 14:44:36 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -1,1 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle 
+import os
 
+st.set_page_config(page_title="Smart Hospital Patient Navigator", page_icon="🏥", layout="wide")
